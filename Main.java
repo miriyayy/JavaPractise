de9af9cf36dc248a18f8384d1d64d90048cb6a16
Miray Sude Mutlu <68467838+miriyayy@users.noreply.github.com>
2024-10-13 14:14:47 +0300

    Burc bulan program

--- Main.java
+++ Main.java
@@ -1,20 +1,45 @@
-/*Java döngüler ile kullanıcının girdiği sayıya kadar çift olan sayıları bulan programı yazıyoruz.*/
-
- import java.util.Scanner;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        int day, month;
 
-        Scanner inp= new Scanner(System.in);
-        int a;
-        System.out.print("Sayı Giriniz:");
-        a= inp.nextInt();
-        int i=1;
-        while(i<=a){
-            i++;
-            if(i%2==0){
-                System.out.println(i);
-            }
+        System.out.println("------------------");
+        System.out.println("Burç Bulan Program");
+        System.out.println("-------------------");
+        System.out.println("Doğum ayını giriniz:");
+        month= input.nextInt();
+         System.out.println("Doğum gününü giriniz:");
+         day= input.nextInt();
+        System.out.println("-------------------");
+        
+        if(month==3 && day>=21 || month==4 && day<=20){
+            System.out.println("Koç Burcu");
+        } else if (month==4 && day>=21|| month ==5 && day<=21){
+            System.out.println("Boğa Burcu");
+
+        } else if (month== 5 && day>=22|| month==6 && day<=22){
+            System.out.println("İkizler Burcu");
+        } else if (month==6 && day>=23|| month==7 && day<=22) {
+            System.out.println("Yengeç Burcu");
+
+        } else if (month== 7 && day >=23 || month==8 && day<=22) {
+            System.out.println("Aslan Burcu");
+        } else if( month== 8 && day>=23|| month==9 && day<= 22){
+            System.out.println("Başak Burcu");
+        } else if (month==9 && day>=23 || month==10 && day <=22) {
+            System.out.println("Terazi Burcu");
+        } else if (month==10 && day>23|| month== 11 && day<=21) {
+            System.out.println("Akrep Burcu");
+        } else if( month==11 && day>=22|| month== 12 && day<=21){
+            System.out.println("Yay Burcu");
+        } else if (month==12 && day>=22|| month== 1 && day<=21) {
+            System.out.println("Oğlak Burcu");
+        } else if(month== 1 && day >=22 || month== 2 && day <=19){
+            System.out.println("Kova Burcu");
+        } else{
+            System.out.println("Balık Burcu");
         }
 
     }
