c52d588e5ac208601d5a9e6ebbd0e0917e8ebc90
Miray Sude Mutlu <68467838+miriyayy@users.noreply.github.com>
2024-10-13 14:16:25 +0300

    artik yil

--- Main.java
+++ Main.java
@@ -2,38 +2,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner= new Scanner(System.in);
-        int birthYear;
-         System.out.println("Please your birth year:");
-         birthYear= scanner.nextInt();
-         String result = "";
-         if(birthYear % 12==0){
-             result="Maymun";
-         } else if (birthYear%12 ==1) {
-             result="Horoz";
-         } else if (birthYear%12==2) {
-             result="Köpek";
-         } else if (birthYear%12 ==3) {
-             result= "Domuz";
-         }else if(birthYear%12 ==4){
-             result="Fare";
-         } else if (birthYear%12==5) {
-             result= "Öküz";
-         } else if (birthYear%12== 6) {
-             result="Kaplan";
-         } else if (birthYear %12==7) {
-             result="Tavşan";
-         } else if (birthYear%12== 8) {
-             result= "Ejderha";
-         } else if (birthYear%12== 9) {
-             result="Yılan";
-         } else if (birthYear%12== 10) {
-             result="At";
-         } else if (birthYear%12==11) {
-             result="Koyun";
-         } else {
-             System.out.println("Wrong Choice!");
-         }
-         System.out.println(result);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(" Please Enter Year: ");
+        int year= scanner.nextInt();
+
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println(year + " is leap year.");
+        } else {
+            System.out.println(year + " is not leap year.");
+        }
+
+        scanner.close();
     }
 }
